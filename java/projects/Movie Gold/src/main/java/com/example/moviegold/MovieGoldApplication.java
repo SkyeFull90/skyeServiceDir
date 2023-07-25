cf66147eb333de1b90565bfff1e0f984325ca359
SkyeFull90 <122103934+SkyeFull90@users.noreply.github.com>
2023-07-24 22:44:37 -0600

    giving java full-stack another try

---
+++ java/projects/Movie Gold/src/main/java/com/example/moviegold/MovieGoldApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviegold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieGoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieGoldApplication.class, args);
+	}
+
+}
