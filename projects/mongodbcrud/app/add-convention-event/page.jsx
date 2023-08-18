9a6f93e0c69da78362646a2f649045b8c7afc820
SkyeFull90 <122103934+SkyeFull90@users.noreply.github.com>
2023-08-17 19:45:31 -0600

    working on setting things up to finalize site

--- projects/mongodbcrud/app/add-convention-event/page.jsx
+++ projects/mongodbcrud/app/add-convention-event/page.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation';
 
 export default function page() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!title || !description) {
+      alert('Tiltle and description are required.');
+      return;
+    }
+
+    try {
+      const res = await fetch('http://localhost:3000/api/topics', {
+        method: "POST",
+        headers: {
+          "Content-type": "appilcation/json"
+        },
+        body: JSON.stringify({title, description}),
+      })
+
+      if (res.ok) {
+        router.push('/')
+      }else{
+        throw new error('Falied to create new topic')
+      }
+
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
   return (
-    <form className='flex flex-col gap-3'>
-        <input className='border border-slate-500 px-8 py-2' type="text" placeholder='Convention Title'/>
-        <input className='border border-slate-500 px-8 py-2' type="text" placeholder='Convention description'/>
-        <button className='btn btn-success py-3 font-bold px-6 w-fit lg:w-80'>Add Convention</button>
+    <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
+        <input 
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        className='border border-slate-500 px-8 py-2' type="text" placeholder='Convention Title'/>
+        <input 
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+        className='border border-slate-500 px-8 py-2' type="text" placeholder='Convention description'/>
+        <button type='submit' className='btn btn-success py-3 font-bold px-6 w-fit lg:w-80'>Add Convention</button>
     </form>
   )
 }
--- projects/mongodbcrud/app/compoents/EventsList.jsx
+++ projects/mongodbcrud/app/compoents/EventsList.jsx
@@ -3,24 +3,43 @@ import RmvBtn from './RmvBtn'
 import Link from 'next/link'
 import { HiPencilAlt }   from "react-icons/hi"
 
-export default function EventsList() {
+const getTopics = async()=>{
+  try {
+  const res = await fetch('http://localhost:3000/api/topics', {
+      cache: 'no-store'
+    })
+
+    if(!res.ok){
+      throw new error("Failed to fetch events");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error laoding events", error)
+  }
+}
+
+export default async function EventsList() {
+  const { topics } = await getTopics();
+
   return (
     <>
-        <main className='card  w-full bg-base-100 border-2 border-slate-800 shadow-xl dark:bg-emerald-800 dark:shadow-cyan-500/70 p-4'>
+      {topics.map(ev => (
+         <main className='card  w-full bg-base-100 border-2 border-slate-800 shadow-xl dark:bg-emerald-800 dark:shadow-cyan-500/70 p-4'>
             <section>
-                <h2 className='card-title font-bold text-2xl text-primary dark:text-white'>Event Title</h2>
-                <p>Description</p>
+                <h2 className='card-title font-bold text-2xl text-primary dark:text-white'>{ev.title}</h2>
+                <p>{ev.description}</p>
             </section>
             <section className='card-actions justify-end gap-2'>
                 <RmvBtn/>
                 <button className='btn btn-outline btn-info'>
-                     <Link href={'/edit-convention/123'} >
+                     <Link href={`/edit-convention/${ev._id}`} >
                         <HiPencilAlt size={24}/>
                      </Link>
-                </button>
-               
+                </button>            
             </section>
         </main>
+      ))}       
     </>
   )
 }
