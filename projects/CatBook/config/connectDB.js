5f3deb34dfdcec4b1dd58dc261c6c5c7bbfa4b0b
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-09-04 23:11:47 -0600

    big update

--- projects/CatBook/config/connectDB.js
+++ projects/CatBook/config/connectDB.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-
+        await mongoose.connect(process.env.DATABASE_URI);
+        console.log("Connected to MongoDB")
     } catch (err) {
         console.log(err)
     }
