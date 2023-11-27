e9ef4c07da5fff4ccf1a11d51a7762e07386989c
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-11-27 10:15:49 -0700

    Add coffee shop functionality and test cases

---
+++ Python/pythonProject/coffeshop.py
@@ -0,0 +1,9 @@
+def coffee_shop():
+    print("Hello welcome to NetworkChuck Coffee!!!!!!")
+
+    name = input("What is your name? ")
+
+    print("Hello " + name + ", thank you so much for coming in today")
+
+# Call the function
+coffee_shop()
