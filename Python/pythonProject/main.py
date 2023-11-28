bf75b1d188caae0711cd232cccd2cea1a222176d
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-11-27 19:41:18 -0700

    Add coffee shop functionality and parameter support

--- Python/pythonProject/main.py
+++ Python/pythonProject/main.py
@@ -6,4 +6,72 @@ print("Hello welcome to NetworkChuck Coffee!!!!!!")
 
 name = input("What is your name? ")
 
-print("Hello " + name + ", thank you so much for coming in today")
+print("Hello " + name + ", thank you so much for coming in today.")
+
+menu = "Black Coffee, White Coffee, Cappuccino, Latte, Espresso, Mocha"
+
+print(name + "what would you like to drink? We have " + menu)
+order = input("What would you like to drink? ")
+
+print("Okay, so you would like a " + order + ", that will be $5.00")
+
+print("Thank you for your order, " + name + ", please pay at the counter and we will call your name when your order is ready.")
+
+print("Order up for " + name + "!!!")
+
+print("Here is your " + order + ", enjoy!")
+
+print("Thank you for coming to NetworkChuck Coffee, " + name + ", have a great day!")
+
+# Now let's build the coffee shop in a function
+
+
+def coffee_shop():
+    print("Hello welcome to NetworkChuck Coffee!!!!!!")
+
+    name = input("What is your name? ")
+
+    print("Hello " + name + ", thank you so much for coming in today.")
+
+    menu = "Black Coffee, White Coffee, Cappuccino, Latte, Espresso, Mocha"
+
+    print(name + "what would you like to drink? We have " + menu)
+    order = input("What would you like to drink? ")
+
+    print("Okay, so you would like a " + order + ", that will be $5.00")
+
+    print("Thank you for your order, " + name + ", please pay at the counter and we will call your name when your order is ready.")
+
+    print("Order up for " + name + "!!!")
+
+    print("Here is your " + order + ", enjoy!")
+
+    print("Thank you for coming to NetworkChuck Coffee, " + name + ", have a great day!")
+    
+# Call the function
+coffee_shop()
+
+# Now let's build the coffee shop in a function with a parameter
+
+
+def coffee_shop(name):
+    
+    print("Hello " + name + ", thank you so much for coming in today.")
+
+    menu = "Black Coffee, White Coffee, Cappuccino, Latte, Espresso, Mocha"
+
+    print(name + "what would you like to drink? We have " + menu)
+    order = input("What would you like to drink? ")
+
+    print("Okay, so you would like a " + order + ", that will be $5.00")
+
+    print("Thank you for your order, " + name + ", please pay at the counter and we will call your name when your order is ready.")
+
+    print("Order up for " + name + "!!!")
+
+    print("Here is your " + order + ", enjoy!")
+
+    print("Thank you for coming to NetworkChuck Coffee, " + name + ", have a great day!")
+    
+# Call the function
+coffee_shop("Chuck")
