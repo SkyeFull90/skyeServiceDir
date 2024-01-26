7d0d6367c11e74325060b7e44879e675d9298fa2
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-01-26 15:03:47 -0700

    Add fetch_one_todo function to database.py

--- projects/farmStack/server/database.py
+++ projects/farmStack/server/database.py
@@ -1,8 +1,13 @@
 import os
 from motor import motor_asyncio
 from dotenv import load_dotenv
+from model import Todo
 
 load_dotenv()
 client = motor_asyncio.AsyncIOMotorClient(os.environ("MONGODB_URI"))
 database = client.TodoList
 colection = database.todo
+
+async def fetch_one_todo(title):
+    document = await colection.find_one({"title": title})
+    return document
