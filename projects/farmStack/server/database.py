85175a8718bcf4701e209d6348dff347a4fd1093
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-01-29 14:02:52 -0700

    Add create_todo and update_todo functions

--- projects/farmStack/server/database.py
+++ projects/farmStack/server/database.py
@@ -13,9 +13,24 @@ async def fetch_one_todo(title):
     document = await collection.find_one({"title": title})
     return document
 
+
 async def fetch_all_todos():
     todos = []
     cursor = collection.find({})
     async for document in cursor:
         todos.append(Todo(**document))
     return todos
+
+
+async def create_todo(todo):
+    document = todo
+    result: object = await collection.insert_one(document)
+    return result
+
+
+async def update_todo(title, desc):
+    await collection.find_one_and_update({"title": title}, {"$set": {
+        "description": desc
+    }})
+    document: object = await collection.find_one({"title": title})
+    return document
