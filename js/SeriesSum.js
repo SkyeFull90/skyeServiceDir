1c26a43b02b5ebd7cc0308e35b2de557987c8a12
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-02-02 23:09:34 -0700

    Add seriesSum function to calculate the sum of a series

--- js/SeriesSum.js
+++ js/SeriesSum.js
@@ -9,3 +9,19 @@ function SeriesSum(n) {
 SeriesSum(5);
 console.log(SeriesSum(5));
 // Path: js/SumOfTheFirstNthTermOfSeries.js
+
+function seriesSum(n) {
+  if (n === 0) {
+    return "0.00";
+  } else {
+    let sum = 0.0;
+    for (let i = 0; i < n; i++) {
+      sum += 1.0 / (1 + i * 3);
+    }
+    return sum.toFixed(2);
+  }
+}
+
+console.log(seriesSum(1));  // "1.00"
+console.log(seriesSum(2));  // "1.25"
+console.log(seriesSum(5));  // "1.57"
