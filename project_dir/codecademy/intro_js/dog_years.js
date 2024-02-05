71f6f42212f72fbe95397f05a7a0167182cdea27
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-02-05 16:07:27 -0700

    Add dog years calculation to JavaScript file

---
+++ project_dir/codecademy/intro_js/dog_years.js
@@ -0,0 +1,6 @@
+// Declare a variable named myAge and set it to the AI's age, which is 1 for simplicity
+let myAge = 1;
+// Declare a variable named earlyYears and set it to 2, representing the first two years of a dog's life
+let earlyYears = 2;
+// Multiply the value of earlyYears by 10.5, as each of the first two years of a dog's life counts as 10.5 dog years
+earlyYears *= 10.5;
