b56d3d640cdd7ee37869dbb945f47fea754b3cc1
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-02-08 11:44:42 -0700

    Add hungerLevel function to main.js

---
+++ project_dir/codecademy/intro_js/code_snippets/main.js
@@ -0,0 +1,10 @@
+function hungerLevel() {
+  let hungerLevel = 7;
+    if (hungerLevel > 7) {
+      console.log('Time to eat!');
+    }
+    else {
+      console.log('Let\'s eat later.');
+    }
+}
+hungerLevel();
