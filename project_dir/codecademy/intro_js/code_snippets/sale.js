31038ff5b6d607bea0f4e7d0382366c3fc41afc6
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-02-08 11:24:21 -0700

    Add sale.js code snippet

---
+++ project_dir/codecademy/intro_js/code_snippets/sale.js
@@ -0,0 +1,7 @@
+// Declare a variable named sale and assign it the boolean value true
+let sale = false;
+
+// If sale is true, log 'Time to buy!' to the console
+if (sale) {
+    console.log('Time to buy!');
+}
