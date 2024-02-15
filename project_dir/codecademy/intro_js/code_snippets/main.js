9232fcd4bd75853dbb003b75b5345be378380f58
taiko <full.skyl@hotmail.com>
2024-02-15 16:04:41 -0700

    Add season function to main.js

--- project_dir/codecademy/intro_js/code_snippets/main.js
+++ project_dir/codecademy/intro_js/code_snippets/main.js
@@ -8,3 +8,19 @@ function hungerLevel() {
     }
 }
 hungerLevel();
+
+function season() {
+  let season = 'summer';
+    if (season === 'spring') {
+      console.log('It\'s spring! The trees are budding!');
+    } else if(season === 'winter') {
+      console.log('It\'s winter! Everything is covered in snow.');
+    } else if(season === 'fall') {
+      console.log('It\'s fall! Leaves are falling!');
+    } else if(season === 'summer') {
+      console.log('It\'s summer! It\'s so hot!');
+    }
+    else {
+      console.log('It\'s not summer.');
+    }
+}
