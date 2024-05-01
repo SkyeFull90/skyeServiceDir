1ded50b70657bd325d43cc9c3680abdead980a8c
skye <full.skyl@hotmail.com>
2024-05-01 13:44:58 -0600

    modified App.jsx to begin to display the tasks list and add a new task

--- project_dir/farmStack_dir/farmStack/frontend/src/App.jsx
+++ project_dir/farmStack_dir/farmStack/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -21,13 +21,19 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className='farm-stack'>
+        <section>
+           <h1>Farm Stack</h1>
+           <h2>Hello World!</h2>
+        </section>
+       <section className=''>
+
+       </section>
+      </div>
     </>
   )
 }
