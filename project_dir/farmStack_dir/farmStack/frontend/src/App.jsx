689aacf5f3bcf10d030322340cd6f9aef241bf44
skye <full.skyl@hotmail.com>
2024-05-02 12:43:43 -0600

    modifed App.jsx for adding in  backend usage

--- project_dir/farmStack_dir/farmStack/frontend/src/App.jsx
+++ project_dir/farmStack_dir/farmStack/frontend/src/App.jsx
@@ -19,36 +19,20 @@ function App() {
         </div>
         <div className="overflow-x-auto">
           <table className="table">
-            {/* head */}
             <thead>
               <tr>
-                <th></th>
-                <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
+                <th>Item</th>
+                <th>Description</th>
+                <th>Edit Todo</th>
+                <th>Delete Todo</th>
               </tr>
             </thead>
             <tbody>
-              {/* row 1 */}
               <tr>
-                <th>1</th>
-                <td>Cy Ganderton</td>
-                <td>Quality Control Specialist</td>
-                <td>Blue</td>
-              </tr>
-              {/* row 2 */}
-              <tr>
-                <th>2</th>
-                <td>Hart Hagerty</td>
-                <td>Desktop Support Technician</td>
-                <td>Purple</td>
-              </tr>
-              {/* row 3 */}
-              <tr>
-                <th>3</th>
-                <td>Brice Swyre</td>
-                <td>Tax Accountant</td>
-                <td>Red</td>
+                <th>Title</th>
+                <td>Description</td>
+                <td><button type="submit" className='btn btn-warning'>Edit</button></td>
+                <td><button type="submit" className='btn btn-error'>Delete</button></td>
               </tr>
             </tbody>
           </table>
