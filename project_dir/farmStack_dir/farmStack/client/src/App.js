452454aa87e9818d45f7154a7268b938f3b6364f
skye <full.skyl@hotmail.com>
2024-05-07 09:03:03 -0600

    modified App.js adding the use state functions for use of axios

--- project_dir/farmStack_dir/farmStack/client/src/App.js
+++ project_dir/farmStack_dir/farmStack/client/src/App.js
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
+import TodoView from './components/TodoListView';
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
 
 function App() {
+  const [todoList, setTodoList] = useState([{}])
+  const [title, setTitle] = useState('')
+  const [desc, setDesc] = useState('')
+
+  //read all the Todos coming in from the server
+
+  //post a todo to the server
+
   return (
     <div className="App">
       <header className="App-header">
